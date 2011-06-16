e5955383e0fe30dfd2be858b5de9ba2f79b7b8db
Ryan Resella <ryan@codeforamerica.org>
2011-06-16 11:33:07 -0700

    Removed webmock

--- lib/connexionz/client.rb
+++ lib/connexionz/client.rb
@@ -6,8 +6,6 @@ module Connexionz
   class Client
     include Connexionz::Client::Connection
     include Connexionz::Client::Request
-    
     include Connexionz::Client::Platforms
-
   end
 end
--- spec/connexionz/platforms_spec.rb
+++ spec/connexionz/platforms_spec.rb
@@ -7,15 +7,9 @@ describe Connexionz::Client::Platforms do
   end
 
   describe ".platform" do
-    before do
-      stub_get("rtt/public/utility/file.aspx?contenttype=SQLXML&Name=Platform.xml").
-        to_return(:status => 200, :body => fixture("platform.xml"))
-    end
-    
     it "should return a list of all the platforms" do
-      test = @client.platform()
-      a_get("rtt/public/utility/file.aspx?contenttype=SQLXML&Name=Platform.xml").should have_been_made
-      test.platforms.platform[0].tag.should == 15
+      test = @client.platform
+      test.platforms.platform[1].platform_no.should == "12412"
     end
   end
 
