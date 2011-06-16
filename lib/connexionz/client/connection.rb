cda2db373537e46de9a08c4ac54ad787574abc22
Ryan Resella <ryan@codeforamerica.org>
2011-06-15 22:32:39 -0700

    Added connection file

---
+++ lib/connexionz/client/connection.rb
@@ -0,0 +1,19 @@
+require 'faraday_middleware'
+
+module Connexionz
+  class Client
+    module Connection
+      private
+
+      def connection
+        Faraday.new(:url => 'http://businfo.santa-clarita.com/') do |connection|
+          connection.use Faraday::Request::UrlEncoded
+          connection.use Faraday::Response::RaiseError
+          connection.use Faraday::Response::Rashify
+          connection.use Faraday::Response::ParseJson
+          connection.adapter(Faraday.default_adapter)
+        end
+      end
+    end
+  end
+end
