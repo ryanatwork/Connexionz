1d44e708dbb1b1e7207c984986cbab52b8c88895
Ryan Resella <ryan@codeforamerica.org>
2011-06-15 22:30:51 -0700

    Updated connexionz.rb to add method missing

--- lib/connexionz.rb
+++ lib/connexionz.rb
@@ -1,3 +1,20 @@
+require 'connexionz/client'
+
 module Connexionz
-  # Your code goes here...
+  # Alias for Connexionz::Client.new
+  #
+  # @return [Connexionz::Client]
+  def self.new
+    Connexionz::Client.new
+  end
+
+  # Delegate to Connexionz::Client
+  def self.method_missing(method, *args, &block)
+    return super unless new.respond_to?(method)
+    new.send(method, *args, &block)
+  end
+
+  def self.respond_to?(method, include_private=false)
+    new.respond_to?(method, include_private) || super(method, include_private)
+  end
 end
