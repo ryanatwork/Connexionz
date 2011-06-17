d44872f5ab62fd478562ea6bf95af8bb6bf9a945
Ryan Resella <ryan@codeforamerica.org>
2011-06-17 14:53:26 -0700

    Added specs for Connexionz

--- spec/connexionz_spec.rb
+++ spec/connexionz_spec.rb
@@ -1,5 +1,20 @@
 require 'helper'
 
 describe Connexionz do
- 
+  after do
+    Connexionz.reset
+  end
+  
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      Connexionz.respond_to?(:client, true).should be_true
+    end
+  end
+
+  describe ".client" do
+    it "should be a Connexionz::Client" do
+      Connexionz.client.should be_a Connexionz::Client
+    end
+  end
+  
 end
